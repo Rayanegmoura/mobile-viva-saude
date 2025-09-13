4e2e5609315cdafbbd15d10e92b101ddc28b2e67
rayanegmoura <rayaneg_moura@hotmail.com>
2025-09-12 21:47:05 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mobile-viva-saude',
+  webDir: 'www'
+};
+
+export default config;
