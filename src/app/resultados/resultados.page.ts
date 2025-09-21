c64245a4b94f6c4611d7c3bcacfa0787fdbafa45
rayanegmoura <rayaneg_moura@hotmail.com>
2025-09-21 20:53:07 -0300

    atualização das páginas do tabs e uma páginas de resultados

---
+++ src/app/resultados/resultados.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resultados',
+  templateUrl: './resultados.page.html',
+  styleUrls: ['./resultados.page.scss'],
+  standalone: false,
+})
+export class ResultadosPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
