5e35c7dd3a0945724e43bf152bb09a9b60208a0f
rayanegmoura <rayaneg_moura@hotmail.com>
2025-09-21 21:22:11 -0300

    excluindo páginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,18 +9,6 @@ const routes: Routes = [
   {
     path: 'tab4',
     loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
-  },
-  {
-    path: 'aluno1',
-    loadChildren: () => import('./aluno1/aluno1.module').then( m => m.Aluno1PageModule)
-  },
-  {
-    path: 'aluno2',
-    loadChildren: () => import('./aluno2/aluno2.module').then( m => m.Aluno2PageModule)
-  },
-  {
-    path: 'aluno3',
-    loadChildren: () => import('./aluno3/aluno3.module').then( m => m.Aluno3PageModule)
   }
 ];
 @NgModule({
